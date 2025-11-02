1da13eabde3ff29da3bfcb106493034d32ccb544
Lucas <lucaspereiraa267@gmail.com>
2025-11-02 13:08:49 -0300

    Alterações Modo Escuro
    Alterei algumas coisas, refiz a "animação" da sidebar e coloquei um botão de verdade para mudar o tema.

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,5 +1,19 @@
-const $html = document.querySelector('html')
-const $checkbox = document.querySelector('#switch')
-$checkbox.addEventListener('change', function(){
-    $html.classList.toggle('dark-mode')
-})
+const html = document.documentElement;
+const btn = document.getElementById('dark-mode');
+
+if (localStorage.getItem('theme') === 'dark') {
+  html.classList.add('dark-mode');
+  btn.textContent = 'Modo Claro'; 
+}
+
+btn.addEventListener('click', () => {
+  html.classList.toggle('dark-mode');
+
+  if (html.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+    btn.textContent = 'Modo Claro';
+  } else {
+    localStorage.setItem('theme', 'light');
+    btn.textContent = 'Modo Escuro';
+  }
+});
